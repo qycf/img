ada3ef498bb9587dbe897aa7989ac0151a8c61a7
lzq <48547293+qycf@users.noreply.github.com>
2021-02-18 20:36:17 +0800

    Upload by PicGo

---
+++ user.php
@@ -0,0 +1,12 @@
+<?php 
+error_reporting(0);
+header("Access-Control-Allow-Origin: *");
+//解析系统 地址
+define('SERVER', 'www.jingyuxitong.com');
+//以上禁止修改 ------------------------------------------------------------------
+
+
+
+$key = 'g9cmKyUCoNhvMsE7O5'; //客户端KEY
+
+
